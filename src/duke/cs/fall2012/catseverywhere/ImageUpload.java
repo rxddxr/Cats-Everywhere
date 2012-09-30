733e2fcdc972febdc0bf45e487c4fdad54fe06af
dalin2 <dma13@duke.edu>
2012-09-30 15:58:34 -0400

    PLEASE WORK

--- src/duke/cs/fall2012/catseverywhere/ImageUpload.java
+++ src/duke/cs/fall2012/catseverywhere/ImageUpload.java
@@ -2,6 +2,7 @@ package duke.cs.fall2012.catseverywhere;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.InputStreamReader;
 
 import org.apache.http.HttpResponse;
@@ -10,6 +11,7 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.ByteArrayBody;
+import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.protocol.BasicHttpContext;
@@ -261,4 +263,44 @@ public class ImageUpload extends Activity {
 		imgView.setImageBitmap(bitmap);
 
 	}
+	
+//	private void doFileUpload(){
+//		 
+//        File file1 = new File(selectedPath1);
+//        File file2 = new File(selectedPath2);
+//        String urlString = "http://10.0.2.2/upload_test/upload_media_test.php";
+//        try
+//        {
+//             HttpClient client = new DefaultHttpClient();
+//             HttpPost post = new HttpPost(urlString);
+//             FileBody bin1 = new FileBody(file1);
+//             FileBody bin2 = new FileBody(file2);
+//             MultipartEntity reqEntity = new MultipartEntity();
+//             reqEntity.addPart("uploadedfile1", bin1);
+//             reqEntity.addPart("uploadedfile2", bin2);
+//             reqEntity.addPart("user", new StringBody("User"));
+//             post.setEntity(reqEntity);
+//             HttpResponse response = client.execute(post);
+//             resEntity = response.getEntity();
+//             final String response_str = EntityUtils.toString(resEntity);
+//             if (resEntity != null) {
+//                 Log.i("RESPONSE",response_str);
+//                 runOnUiThread(new Runnable(){
+//                        public void run() {
+//                             try {
+//                                res.setTextColor(Color.GREEN);
+//                                res.setText("n Response from server : n " + response_str);
+//                                Toast.makeText(getApplicationContext(),"Upload Complete. Check the server uploads directory.", Toast.LENGTH_LONG).show();
+//                            } catch (Exception e) {
+//                                e.printStackTrace();
+//                            }
+//                           }
+//                    });
+//             }
+//        }
+//        catch (Exception ex){
+//             Log.e("Debug", "error: " + ex.getMessage(), ex);
+//        }
+//      }
+//}
 }
---
+++ src/duke/cs/fall2012/catseverywhere/database/GetImages.java
@@ -0,0 +1,9 @@
+package duke.cs.fall2012.catseverywhere.database;
+
+public class GetImages {
+
+	//Used to get images from database
+	//use php to get paths from database
+	//use paths in java to get image from online.
+	
+}
