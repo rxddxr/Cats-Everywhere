30303f30b698667b7d4a18057b26e800c1435129
Antares Yee <antaresyee@gmail.com>
2012-09-30 15:04:15 -0400

    gen

--- gen/duke/cs/fall2012/catseverywhere/R.java
+++ gen/duke/cs/fall2012/catseverywhere/R.java
@@ -11,9 +11,15 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int splash_background=0x7f020002;
+        public static final int buds=0x7f020000;
+        public static final int cherry_34=0x7f020001;
+        public static final int clouds_2=0x7f020002;
+        public static final int coffee_beans_2=0x7f020003;
+        public static final int death_valley_sand_dunes=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int morning_glory_pool=0x7f020007;
+        public static final int splash_background=0x7f020008;
     }
     public static final class id {
         public static final int Caption=0x7f08000c;
