4cf2537b902200ac42879b641f7f58dd7fcdbda2
Roberto D <john@wl-10-190-55-213.wireless.duke.local>
2012-10-16 16:51:13 -0400

    Working on keyword upload

--- src/duke/cs/fall2012/catseverywhere/ImageUpload.java
+++ src/duke/cs/fall2012/catseverywhere/ImageUpload.java
@@ -290,8 +290,7 @@ public class ImageUpload extends Activity {
 			MultipartEntity reqEntity = new MultipartEntity();
 			reqEntity.addPart("uploadedFile", bin1);
 			reqEntity.addPart("id", new StringBody(getId(file1)));
-			StringBody keywords = new StringBody(caption.getText().toString());
-			System.out.println("keywords: " + keywords);
+			System.out.println("keywords: " +caption.getText().toString());
 			reqEntity.addPart("keywords", new StringBody(caption.getText().toString()));
 
 			// UPDATE THIS TO ADD OWNER, LOCATION, KEYWORD DATA TO DB
