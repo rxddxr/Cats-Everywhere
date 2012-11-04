8f2b66ce7a8b39aa72ec056f4c173db3ba530f96
Roberto D <john@wl-10-190-49-4.wireless.duke.local>
2012-11-04 14:20:01 -0500

    Removed unused Files

--- src/duke/cs/fall2012/catseverywhere/database/GetImages.java
+++
@@ -1,9 +0,0 @@
-package duke.cs.fall2012.catseverywhere.database;
-
-public class GetImages {
-
-	//Used to get images from database
-	//use php to get paths from database
-	//use paths in java to get image from online.
-	
-}
