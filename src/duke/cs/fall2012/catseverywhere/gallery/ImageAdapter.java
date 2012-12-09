0090672f570704e1a9082aab279e97a761ea2a02
dalin2 <dma13@duke.edu>
2012-12-09 11:49:39 -0500

    adapters inheritance

---
+++ src/duke/cs/fall2012/catseverywhere/gallery/ImageAdapter.java
@@ -0,0 +1,49 @@
+package duke.cs.fall2012.catseverywhere.gallery;
+
+import duke.cs.fall2012.catseverywhere.R;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public abstract class ImageAdapter extends BaseAdapter {
+
+    protected Activity activity;
+    private String[] data;
+    protected static LayoutInflater inflater = null;
+    public ImageLoader imageLoader;
+    
+    public ImageAdapter(Activity a, String[] d) {
+        activity = a;
+        data=d;
+        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+    
+	public int getCount() {
+		return data.length;
+	}
+
+	public Object getItem(int position) {
+		return position;
+	}
+
+	public long getItemId(int position) {
+		return position;
+	}
+	
+	public abstract View inflate();
+	
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View vi = convertView;
+		if (convertView == null)
+			vi = inflate();
+
+		ImageView image = (ImageView) vi.findViewById(R.id.image);
+		imageLoader.displayImage(data[position], image);
+		return vi;
+	}
+    
+}
--- src/duke/cs/fall2012/catseverywhere/gallery/ImageGalleryAdapter.java
+++ src/duke/cs/fall2012/catseverywhere/gallery/ImageGalleryAdapter.java
@@ -2,47 +2,17 @@ package duke.cs.fall2012.catseverywhere.gallery;
 
 import duke.cs.fall2012.catseverywhere.R;
 import android.app.Activity;
-import android.content.Context;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.ImageView;
 
-public class ImageGalleryAdapter extends BaseAdapter {
+public class ImageGalleryAdapter extends ImageAdapter {
 
-	private Activity activity;
-    private String[] data;
-    private static LayoutInflater inflater=null;
-    public ImageLoader imageLoader; 
-    
-    public ImageGalleryAdapter(Activity a, String[] d) {
-        activity = a;
-        data= d;
-        inflater = (LayoutInflater)activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        imageLoader= new ImageLoader(activity.getApplicationContext(), "gallery");
-    }
+	public ImageGalleryAdapter(Activity a, String[] d) {
+		super(a, d);
+		imageLoader = new ImageLoader(activity.getApplicationContext(), "gallery");
+	}
 
-    public int getCount() {
-        return data.length;
-    }
+	public View inflate() {
+		return inflater.inflate(R.layout.item_gallery_image, null);
+	}
 
-    public Object getItem(int position) {
-        return position;
-    }
-
-    public long getItemId(int position) {
-        return position;
-    }
-    
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View vi=convertView;
-        if(convertView==null)
-            vi = inflater.inflate(R.layout.item_gallery_image, null);
-
-        ImageView image=(ImageView)vi.findViewById(R.id.image);
-        imageLoader.displayImage(data[position], image);
-        return vi;
-    }
-    
 }
